56e1dd0f35dc4d19c6614cbe3cf9552a30ea3e0b
Sammybett-bit <bettsammy20@gmail.com>
2023-07-12 13:57:15 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,22 @@
+const divide = function() {
+    return 2000 / 100;
+  };
+  
+  divide(2000 / 100);
+  console.log( divide(2000 / 100));
+
+  const numbers = [4, 3];
+  const square = (number) => number * number;
+  const squaredNumbers = numbers.map(square);
+  const add = (a, b) => {
+    return a + b;
+  };
+  
+  let a = 3;
+  let b = 4;
+  
+  let result = add(a, b);
+  console.log(result); 
+  
+
+  console.log(squaredNumbers); 
